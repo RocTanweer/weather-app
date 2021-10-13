6fdb728e39fb1a5c2acfadeb1b970ca6d1c717bd
RocTanweer <mohammadtanweer769@gmail.com>
2021-10-13 10:25:35 +0530

    Add: Eventlistener on window using for conditional rendering | styling for responsiveness

--- client/src/js/layouts/content/Content.js
+++ client/src/js/layouts/content/Content.js
@@ -1,36 +1,49 @@
 import Button from "../../components/global/Button";
 import { GlobalContext } from "../../store/context-manager";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import ContentForecast from "./ContentForecast";
 import Loading from "../../components/global/Loading";
 import ContentHighlights from "./ContentHighlights";
 
 function Content() {
   const { isLoading, isCelcius, handleButtonClick } = useContext(GlobalContext);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const resizeEventFunc = (e) => {
+      setWindowWidth(e.target.innerWidth);
+    };
+    window.addEventListener("resize", resizeEventFunc);
+    return () => {
+      window.removeEventListener("resize", resizeEventFunc);
+    };
+  }, []);
 
   return (
     <section className="content">
       <div className="contentWrapper">
-        <div className="content__top">
-          <div className="switchTemp">
-            <Button
-              isLoading={isLoading}
-              type="degC"
-              active={isCelcius}
-              handleClick={handleButtonClick}
-            >
-              °C
-            </Button>
-            <Button
-              isLoading={isLoading}
-              type="degF"
-              active={!isCelcius}
-              handleClick={handleButtonClick}
-            >
-              °F
-            </Button>
+        {windowWidth > 375 && (
+          <div className="content__top">
+            <div className="switchTemp">
+              <Button
+                isLoading={isLoading}
+                type="degC"
+                active={isCelcius}
+                handleClick={handleButtonClick}
+              >
+                °C
+              </Button>
+              <Button
+                isLoading={isLoading}
+                type="degF"
+                active={!isCelcius}
+                handleClick={handleButtonClick}
+              >
+                °F
+              </Button>
+            </div>
           </div>
-        </div>
+        )}
         <section className="content__forecastWrapper">
           {isLoading ? <Loading size={"sm"} /> : <ContentForecast />}
         </section>
